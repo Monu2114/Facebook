9f569dc463437b02ac47ddd6521a816fc624565a
Monu_2114 <monu.nanabala@gmail.com>
2024-08-31 22:25:44 +0530

    router , vercel errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
-import tailwindcss from "tailwindcss";
-
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"],
     },
   },
 });
